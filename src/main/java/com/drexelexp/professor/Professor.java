1ea1579fb8407f7c4226cf8956548846f2418b9c
Chris Harvey <charvey@drexel.edu>
2012-08-27 18:31:52 -0400

    Created Professor model

--- src/main/java/com/drexelexp/professor/Professor.java
+++ src/main/java/com/drexelexp/professor/Professor.java
@@ -5,5 +5,28 @@ package com.drexelexp.professor;
  *
  */
 public class Professor {
+	private int id;
+	private String name;
+	
+	public Professor(int id, String name){
+		this.id=id;
+		this.name=name;
+	}
+	
+	public int getId() {
+		return id;
+	}
 
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName(){
+		return name;
+	}
+	
+	public void setName(String name){
+		this.name=name;
+	}
+	
 }
