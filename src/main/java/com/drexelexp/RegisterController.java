543ad25015d4fc66079d5a71339df094753a4e5f
Timothy Hahn <tyh25@drexel.edu>
2012-08-28 19:00:19 -0400

    and solved conflict

--- src/main/java/com/drexelexp/RegisterController.java
+++ src/main/java/com/drexelexp/RegisterController.java
@@ -9,20 +9,7 @@ import com.drexelexp.user.User;
 
 @Controller
 public class RegisterController {
-	
-<<<<<<< HEAD
 
-=======
-	@RequestMapping(value="/create_user", method = RequestMethod.POST)
-	public String create_user(@ModelAttribute("user")
-    User user,ModelMap model) {
-		ApplicationContext context = 
-	    		new ClassPathXmlApplicationContext("Spring-Module.xml");
-		BaseDAO<User> userDAO = (JdbcUserDAO) context.getBean("userDAO");
-		userDAO.insert(user);
-		return "login";
-	}
->>>>>>> 4759239d7bd1ae63ad53bd767f61c06edb4ad3fb
 	
 	@RequestMapping(value="/register")
 	public ModelAndView showUsers() {
