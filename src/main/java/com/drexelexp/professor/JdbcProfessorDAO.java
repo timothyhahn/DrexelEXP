c335de19c8cf4bb3a9cae130429885b847ee7a94
Timothy Hahn <tyh25@drexel.edu>
2012-09-01 16:30:14 -0400

    restored missing professor files

---
+++ src/main/java/com/drexelexp/professor/JdbcProfessorDAO.java
@@ -0,0 +1,207 @@
+package com.drexelexp.professor;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+import com.drexelexp.baseDAO.BaseDAO;
+import com.drexelexp.baseDAO.JdbcDAO;
+
+/**
+ * Implementation of ProfessorDAO in JDBC style.
+ * @author
+ *
+ */
+public class JdbcProfessorDAO extends JdbcDAO implements BaseDAO<Professor>{
+
+	@Override
+	public void insert(Professor instance) {
+		String sql = "INSERT INTO PROFESSORS " + "(NAME) VALUES (?)";
+		Connection conn = null;
+ 
+		try {
+			conn = dataSource.getConnection();
+			PreparedStatement ps = conn.prepareStatement(sql);
+			ps.setString(1, instance.getName());
+			ps.executeUpdate();
+			ps.close();
+ 
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+ 
+		} finally {
+			if (conn != null) {
+				try {
+					conn.close();
+				} catch (SQLException e) {}
+			}
+		}
+	}
+
+	public ArrayList<Professor> getAll() {
+		ArrayList<Professor> professors = new ArrayList<Professor>();
+		String sql = "SELECT * FROM PROFESSORS";
+		Connection conn = null;
+		 
+		try {
+			conn = dataSource.getConnection();
+			PreparedStatement ps = conn.prepareStatement(sql);
+	
+			ResultSet rs = ps.executeQuery();
+
+				while (rs.next()) {
+					Professor professor = null;
+					professor = new Professor(
+						rs.getInt("PROF_ID"),
+						rs.getString("NAME")
+					);
+					professors.add(professor);
+				}
+			rs.close();
+			ps.close();
+			return professors;
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+		} finally {
+			if (conn != null) {
+				try {
+				conn.close();
+				} catch (SQLException e) {}
+			}
+		}
+		
+	}
+	@Override
+	public Professor getById(int id) {
+		String sql = "SELECT * FROM PROFESSORS WHERE PROF_ID = ?";
+		 
+		Connection conn = null;
+ 
+		try {
+			conn = dataSource.getConnection();
+			PreparedStatement ps = conn.prepareStatement(sql);
+			ps.setInt(1, id);
+			Professor professor = null;
+			ResultSet rs = ps.executeQuery();
+			if (rs.next()) {
+				professor = new Professor(
+					rs.getInt("PROF_ID"),
+					rs.getString("NAME")
+				);
+			}
+			
+			rs.close();
+			ps.close();
+			return professor;
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+		} finally {
+			if (conn != null) {
+				try {
+				conn.close();
+				} catch (SQLException e) {}
+			}
+		}
+	}
+
+	public void edit(Professor professor) {
+		String sql = "UPDATE PROFESSORS SET NAME = ? WHERE PROF_ID = ?";
+		 
+		Connection conn = null;
+ 
+		try {
+			System.out.println(sql);
+			conn = dataSource.getConnection();
+			PreparedStatement ps = conn.prepareStatement(sql);
+			ps.setString(1, professor.getName());
+			ps.setInt(2, professor.getId());
+			System.out.println(ps.toString());
+			ps.executeUpdate();
+			ps.close();
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+		} finally {
+			if (conn != null) {
+				try {
+				conn.close();
+				} catch (SQLException e) {}
+			}
+		
+		}
+	}
+
+	public void delete(Professor professor) {
+		String sql = "DELETE FROM PROFESSORS WHERE PROF_ID = ?";
+		 
+		Connection conn = null;
+ 
+		try {
+			System.out.println(sql);
+			conn = dataSource.getConnection();
+			PreparedStatement ps = conn.prepareStatement(sql);
+			ps.setInt(1, professor.getId());
+			System.out.println(ps.toString());
+			ps.executeUpdate();
+			ps.close();
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+		} finally {
+			if (conn != null) {
+				try {
+				conn.close();
+				} catch (SQLException e) {}
+			}
+		
+		}
+	}
+	public LinkedList<Professor> searchByName(ArrayList<String> queryTerms) {
+		LinkedList<Professor> professors = new LinkedList<Professor>();
+		for(String s : queryTerms) {
+			String sql = "SELECT * FROM PROFESSORS WHERE NAME LIKE ?";
+			 
+			Connection conn = null;
+	 
+			try {
+				conn = dataSource.getConnection();
+				PreparedStatement ps = conn.prepareStatement(sql);
+				ps.setString(1, "%" + s + "%");
+				ResultSet rs = ps.executeQuery();
+				while (rs.next()) {
+
+					Professor professor = null;
+					
+					professor = new Professor(
+						rs.getInt("PROF_ID"),
+						rs.getString("NAME")
+					);
+					boolean profNotFound = true;
+					for(Professor p : professors) {
+						if(p.getId() == professor.getId()) {
+							professors.remove(p);
+							professors.addFirst(p);
+							profNotFound = false;
+							System.out.println("exists!");
+						}
+					}
+					if(profNotFound)
+						professors.add(professor);
+				}
+				rs.close();
+				ps.close();
+			} catch (SQLException e) {
+				throw new RuntimeException(e);
+			} finally {
+				if (conn != null) {
+					try {
+					conn.close();
+					} catch (SQLException e) {}
+				}
+			}
+		}
+		
+		return professors;
+	}
+}
---
+++ src/main/java/com/drexelexp/professor/ProfessorController.java
@@ -0,0 +1,163 @@
+package com.drexelexp.professor;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.drexelexp.baseDAO.BaseDAO;
+
+/**
+ * Controller for the Professor object
+ * @author
+ *
+ */
+@Controller
+public class ProfessorController {
+	
+	@RequestMapping(value="/professor/add", method = RequestMethod.GET)
+	public ModelAndView addProfessor() {
+		return new ModelAndView("professor/add", "command", new Professor());
+	}
+	
+	@RequestMapping(value="/professor/create", method = RequestMethod.POST)
+	public String createProfessor(@ModelAttribute("professor") Professor professor, ModelMap model) {
+		ApplicationContext context = 
+	    		new ClassPathXmlApplicationContext("Spring-Module.xml");
+		BaseDAO<Professor> professorDAO = (JdbcProfessorDAO) context.getBean("professorDAO");
+		professorDAO.insert(professor);
+		return "professor/list";
+	}
+	
+	@RequestMapping(value="/professor/edit", method = RequestMethod.GET) 
+	public String listEditProfessor(Model model) {
+		ArrayList<Professor> professors = null;
+		ApplicationContext context = 
+	    		new ClassPathXmlApplicationContext("Spring-Module.xml");
+		BaseDAO<Professor> professorDAO = (JdbcProfessorDAO) context.getBean("professorDAO");
+		
+		professors = ((JdbcProfessorDAO) professorDAO).getAll();
+		
+		model.addAttribute("professors",professors);
+		
+		return "professor/edit/list";
+	}
+	
+	@RequestMapping(value="/professor/edit/{profID}", method = RequestMethod.GET) 
+	public ModelAndView editProfessor(@PathVariable String profID, Model model) {
+
+		System.out.println("ID: " + profID);
+		ApplicationContext context = 
+	    		new ClassPathXmlApplicationContext("Spring-Module.xml");
+		BaseDAO<Professor> professorDAO = (JdbcProfessorDAO) context.getBean("professorDAO");
+		Professor professor = new Professor();
+		professor.setId(Integer.parseInt(profID));
+		professor = ((JdbcProfessorDAO) professorDAO).getById(professor.getId());
+		model.addAttribute("professor", professor);
+		return new ModelAndView("professor/edit", "command", professor);
+	}
+	
+	@RequestMapping(value="/professor/edit/{profID}", method = RequestMethod.POST)
+	public ModelAndView updateProfessor(@ModelAttribute("professor") Professor professor, @PathVariable String profID) {
+		professor.setId(Integer.parseInt(profID));
+		ApplicationContext context = 
+	    		new ClassPathXmlApplicationContext("Spring-Module.xml");
+		BaseDAO<Professor> professorDAO = (JdbcProfessorDAO) context.getBean("professorDAO");
+		((JdbcProfessorDAO)professorDAO).edit(professor);
+		return new ModelAndView("redirect:../");
+	}
+	
+	@RequestMapping(value="/professor/delete", method = RequestMethod.GET) 
+	public String listDeleteProfessor(Model model) {
+		ArrayList<Professor> professors = new ArrayList<Professor>();
+		ApplicationContext context = 
+	    		new ClassPathXmlApplicationContext("Spring-Module.xml");
+		BaseDAO<Professor> professorDAO = (JdbcProfessorDAO) context.getBean("professorDAO");
+		
+		professors = ((JdbcProfessorDAO) professorDAO).getAll();
+		
+		model.addAttribute("professors",professors);
+		
+		return "delete/list";
+	}
+	
+	@RequestMapping(value="/professor/delete/{profID}", method = RequestMethod.GET) 
+	public ModelAndView deleteProfessor(@PathVariable String profID, Model model) {
+		System.out.println("ID: " + profID);
+		ApplicationContext context = 
+	    		new ClassPathXmlApplicationContext("Spring-Module.xml");
+		BaseDAO<Professor> professorDAO = (JdbcProfessorDAO) context.getBean("professorDAO");
+		Professor professor = new Professor();
+		professor.setId(Integer.parseInt(profID));
+		professor = ((JdbcProfessorDAO) professorDAO).getById(professor.getId());
+		model.addAttribute("professor", professor);
+		return new ModelAndView("professor/delete/confirm", "command", professor);
+	}
+	
+	@RequestMapping(value="/professor/delete/{profID}", method = RequestMethod.POST)
+	public ModelAndView removeProfessor(@ModelAttribute("professor") Professor professor, @PathVariable String profID) {
+		professor.setId(Integer.parseInt(profID));
+		ApplicationContext context = 
+	    		new ClassPathXmlApplicationContext("Spring-Module.xml");
+		BaseDAO<Professor> professorDAO = (JdbcProfessorDAO) context.getBean("professorDAO");
+		((JdbcProfessorDAO)professorDAO).delete(professor);
+		return new ModelAndView("redirect:../");
+	}
+	
+	@RequestMapping(value="/professor", method = RequestMethod.GET)
+	public String list(Model model) {
+		ArrayList<Professor> professors = null;
+		
+		ApplicationContext context = 
+	    		new ClassPathXmlApplicationContext("Spring-Module.xml");
+		BaseDAO<Professor> professorDAO = (JdbcProfessorDAO) context.getBean("professorDAO");
+		
+		professors = ((JdbcProfessorDAO) professorDAO).getAll();
+		
+		model.addAttribute("professors",professors);
+		
+		return "professor/list";
+	}
+	
+	
+	
+	@RequestMapping(value="/professor/show/{profID}", method = RequestMethod.GET)
+	public String show(@PathVariable String profID, Model model) {
+		ApplicationContext context = new ClassPathXmlApplicationContext("Spring-Module.xml");
+		BaseDAO<Professor> dao = (JdbcProfessorDAO) context.getBean("professorDAO");
+		
+		model.addAttribute("professor",dao.getById(Integer.parseInt(profID)));
+		
+		return "professor/show";
+	}
+	
+	@RequestMapping(value="/professor/search", method = RequestMethod.GET)
+	public ModelAndView searchForProfessor() {
+		return new ModelAndView("professor/search", "command", new Professor());
+		
+	}
+	@RequestMapping(value="/professor/search", method = RequestMethod.POST)
+	public String showSearchResults(@ModelAttribute("name") Professor p, Model model) {
+		String query = p.getName();
+		
+		ArrayList<String> queryList = new ArrayList<String>(Arrays.asList(query.split(" ")));
+		ApplicationContext context = new ClassPathXmlApplicationContext("Spring-Module.xml");
+		JdbcProfessorDAO dao = (JdbcProfessorDAO) context.getBean("professorDAO");
+		ArrayList<Professor> professors = new ArrayList<Professor>();
+		
+		professors = new ArrayList<Professor>(dao.searchByName(queryList));
+		
+		
+		model.addAttribute("professors",professors);
+		return "professor/list";
+	}
+}
