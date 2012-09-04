d4a6837fd43f5d55a9a16da583fc8be71ec8be49
Martin Barbella <msb84@drexel.edu>
2012-09-03 20:46:41 -0400

    Remove unused imports from professor test

--- src/main/java/com/drexelexp/test/ProfessorTest.java
+++ src/main/java/com/drexelexp/test/ProfessorTest.java
@@ -2,17 +2,13 @@ package com.drexelexp.test;
 
 import static org.junit.Assert.*;
 
-import org.junit.After;
-import org.junit.Before;
 import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-import org.springframework.web.servlet.ModelAndView;
 
 import com.drexelexp.baseDAO.BaseDAO;
 import com.drexelexp.professor.JdbcProfessorDAO;
 import com.drexelexp.professor.Professor;
-import com.drexelexp.professor.ProfessorController;
 
 public class ProfessorTest {
 
