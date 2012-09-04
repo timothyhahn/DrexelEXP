f4bd24a5848082f8a3c809cdaf428f5bd14ba2d6
Martin Barbella <msb84@drexel.edu>
2012-09-03 20:43:31 -0400

    Remove controller tests

--- src/main/java/com/drexelexp/test/ProfessorTest.java
+++ src/main/java/com/drexelexp/test/ProfessorTest.java
@@ -2,14 +2,12 @@ package com.drexelexp.test;
 
 import static org.junit.Assert.*;
 
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-import org.springframework.mock.web.MockHttpServletRequest;
-import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.mvc.annotation.AnnotationMethodHandlerAdapter;
 
 import com.drexelexp.baseDAO.BaseDAO;
 import com.drexelexp.professor.JdbcProfessorDAO;
@@ -17,35 +15,18 @@ import com.drexelexp.professor.Professor;
 import com.drexelexp.professor.ProfessorController;
 
 public class ProfessorTest {
-    private MockHttpServletRequest request;
-    private MockHttpServletResponse response;
-    private ProfessorController controller;
-
-    @Before
-    public void setUp() {
-       request = new MockHttpServletRequest();
-       response = new MockHttpServletResponse();
-       controller = new ProfessorController();
-    }
 
 	@Test
 	public void createProfessorTest() throws Exception {
-		request.setRequestURI("/professor/create");
-		request.setMethod("POST");
-		Professor professor = new Professor(9999, "Professor Test");
-		request.setAttribute("professor", professor);
-		
-		ModelAndView mav = new AnnotationMethodHandlerAdapter().handle(request, response, controller);
-		assertEquals("redirect:/professor", mav.getViewName());
+		Professor professor = new Professor(-1, "Professor Test");
 		
 		ApplicationContext context = 
 	    		new ClassPathXmlApplicationContext("Spring-Module.xml");
 		BaseDAO<Professor> professorDAO = (JdbcProfessorDAO) context.getBean("professorDAO");
-		Professor result = ((JdbcProfessorDAO)professorDAO).getById(9999);
+		((JdbcProfessorDAO)professorDAO).insert(professor);
 		
-		assertEquals("Professor Test", result.getName());
-		
-		((JdbcProfessorDAO)professorDAO).delete(professor);
+		Professor result = ((JdbcProfessorDAO)professorDAO).getByName("Professor Test");
+		((JdbcProfessorDAO)professorDAO).delete(result);
 	}
 
 }
